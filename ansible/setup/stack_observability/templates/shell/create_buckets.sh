5e96a12b5de9106cd56461e65f99203f93f09c60
VURU <sueleyman.vurucu@amova.eu>
2023-01-31 12:46:50 +0100

    nomadder-96 Minio as LGTM storage. Create bucket as poststart condition

--- ansible/setup/stack_observability/templates/shell/create_buckets.sh
+++ ansible/setup/stack_observability/templates/shell/create_buckets.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
-++- range  $index, $service := service "minio" -++
-++- if eq $index 0 ++
-minioserver="minio.service.consul:++ $service.Port ++"
-++- end ++
-++- end ++
-echo $minioserver > /tmp/test.txt
-mc config host add myminio http://$minioserver $MINIO_USER $MINIO_PASSWORD
-mc mb myminio/$MINIO_BUCKET
-mc policy set public myminio/$MINIO_BUCKET
-exit 0
+
+addserver(){
+    ++- range  $index, $service := service "minio" -++
+    ++- if eq $index 0 ++
+    minioserver="minio.service.consul:++ $service.Port ++"
+    ++- end ++
+    ++- end ++
+    mc config host add myminio http://$minioserver $MINIO_USER $MINIO_PASSWORD
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't add minio server. Return code is: $result"
+      return $result
+    fi
+}
+
+createbucket(){
+    mc mb --ignore-existing myminio/$MINIO_BUCKET
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't create minio bucket. Return code is: $result"
+      return $result
+    fi
+}
+
+addpolicy(){
+    mc policy set public myminio/$MINIO_BUCKET
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't set public policy. Return code is: $result "
+      return $result
+    fi
+}
+
+addserver
+createbucket
+addpolicy
