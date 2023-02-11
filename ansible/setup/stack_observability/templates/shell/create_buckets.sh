01d334a2aecdc6c7c21b2dc1859ea667a9d56492
Süleyman Vurucu <S_Vurucu@hotmail.com>
2023-02-11 12:37:12 +0100

    Nomadder 96 (#101)
    * nomadder-96 Minio as LGTM storage. Deploy minio and install mc
    
    * nomadder-96 Minio as LGTM storage. Minio host volume
    
    * nomadder-96 Minio as LGTM storage. Minio Prometheus scrape
    
    * nomadder-96 Minio as LGTM storage. Minio metrics
    
    * nomadder-96 Minio as LGTM storage. Minio metrics
    
    * nomadder-96 Minio as LGTM storage. Migrate from nexus ftp upload to local file templating
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Create bucket as poststart condition
    
    * nomadder-96 Minio as LGTM storage. Loki performance
    
    * nomadder-96 Minio as LGTM storage. Loki ruler
    
    * nomadder-96 Minio as LGTM storage. Loki data retention
    
    * nomadder-96 Minio as LGTM storage. Loki data retention
    
    * nomadder-96 Minio as LGTM storage. Loki data retention
    
    * nomadder-96 Minio as LGTM storage. Loki data retention
    
    * nomadder-96 Minio as LGTM storage. Loki data retention
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    * nomadder-96 Minio as LGTM storage. Grafana agent node and deployment update
    
    ---------
    
    Co-authored-by: VURU <sueleyman.vurucu@amova.eu>

---
+++ ansible/setup/stack_observability/templates/shell/create_buckets.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+addserver(){
+    ++- range  $index, $service := service "minio" -++
+    ++- if eq $index 0 ++
+    minioserver="minio.service.consul:++ $service.Port ++"
+    ++- end ++
+    ++- end ++
+    mc config host add myminio http://$minioserver $MINIO_USER $MINIO_PASSWORD
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't add minio server. Return code is: $result"
+      return $result
+    fi
+}
+
+createbucket(){
+    mc mb --ignore-existing myminio/$MINIO_BUCKET
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't create minio bucket. Return code is: $result"
+      return $result
+    fi
+}
+
+addpolicy(){
+    mc anonymous set public myminio/$MINIO_BUCKET
+    result=$?
+    if [ $result -gt 0 ]; then
+      echo "Can't set public policy. Return code is: $result "
+      return $result
+    fi
+}
+
+addserver
+createbucket
+addpolicy
