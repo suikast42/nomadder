ad6744fde3dc9eb359e2eb0ef68dbf410108bffc
VURU <sueleyman.vurucu@amova.eu>
2025-02-02 15:19:54 +0100

    Version updates: nexus.

---
+++ ansible/environment/local_devops/setenv.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+
+export DOCKER_HOST=192.168.30.31
+export TLS_SAN=tdp.private
+export ENVIRONMENT="/mnt/c/IDE/Projects_Git/playground/nomadder/ansible/environment/local_devops"
+export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
+export ANSIBLE_INVENTORY="$ENVIRONMENT/inventory/hosts.ini"
+export ANSIBLE_DEBUG=False
+export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
+export PULL_REGISTRY="registry.$TLS_SAN"
+export PUSH_REGISTRY="$DOCKER_HOST:5001"
+
+
+export DOCKER_HOST=192.168.30.31
+export TLS_SAN=tdp.private
+export ENVIRONMENT="/home/cloudmaster/nomadder/ansible/environment/local_devops"
+export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
+export ANSIBLE_INVENTORY="$ENVIRONMENT/inventory/hosts.ini"
+export ANSIBLE_DEBUG=False
+export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
+export PULL_REGISTRY="registry.$TLS_SAN"
+export PUSH_REGISTRY="$DOCKER_HOST:5001"
---
+++ vagrant/ubuntu/24.04_esxi/set_env.bat
@@ -0,0 +1,3 @@
+set ESXI_HOST=192.168.30.240
+set ESXI_USER=root
+set ESXI_PASS=your pass here
