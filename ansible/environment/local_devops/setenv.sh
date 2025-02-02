ee785b62464c11aae2265f0de6477944384d4ed3
VURU <sueleyman.vurucu@amova.eu>
2025-02-02 15:24:19 +0100

    Version updates: nexus.

--- ansible/environment/local_devops/setenv.sh
+++ ansible/environment/local_devops/setenv.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
-export DOCKER_HOST=192.168.30.31
+export DOCKER_HOST=192.168.30.121
 export TLS_SAN=tdp.private
 export ENVIRONMENT="/mnt/c/IDE/Projects_Git/playground/nomadder/ansible/environment/local_devops"
 export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
@@ -11,7 +11,7 @@ export PULL_REGISTRY="registry.$TLS_SAN"
 export PUSH_REGISTRY="$DOCKER_HOST:5001"
 
 
-export DOCKER_HOST=192.168.30.31
+export DOCKER_HOST=192.168.30.121
 export TLS_SAN=tdp.private
 export ENVIRONMENT="/home/cloudmaster/nomadder/ansible/environment/local_devops"
 export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
