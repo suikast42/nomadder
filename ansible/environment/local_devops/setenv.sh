5d49e130b77ac0abaafdaa94103266e1ed7f30d8
VURU <sueleyman.vurucu@amova.eu>
2025-02-06 20:55:58 +0100

    version updates: trafik, grafana,grafana_agent. Improve mimir metrics with common scheme: service_id,exporter_id,version,version_exporter,environment,organisation

--- ansible/environment/local_devops/setenv.sh
+++ ansible/environment/local_devops/setenv.sh
@@ -9,14 +9,3 @@ export ANSIBLE_DEBUG=False
 export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
 export PULL_REGISTRY="registry.$TLS_SAN"
 export PUSH_REGISTRY="$DOCKER_HOST:5001"
-
-
-export DOCKER_HOST=192.168.30.121
-export TLS_SAN=tdp.private
-export ENVIRONMENT="/home/cloudmaster/nomadder/ansible/environment/local_devops"
-export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
-export ANSIBLE_INVENTORY="$ENVIRONMENT/inventory/hosts.ini"
-export ANSIBLE_DEBUG=False
-export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
-export PULL_REGISTRY="registry.$TLS_SAN"
-export PUSH_REGISTRY="$DOCKER_HOST:5001"
