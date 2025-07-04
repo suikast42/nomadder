b7e7a18bdc3305684f5f09a14c5f3da22a824305
Süleyman Vurucu <S_Vurucu@hotmail.com>
2025-07-04 09:06:00 +0200

    Version updates. (#170)
    Delete docker-compose installation in favor of docker compose plugin
    
    Co-authored-by: VURU <sueleyman.vurucu@amova.eu>

--- ansible/setup/registry/templates/startCompose.sh
+++ ansible/setup/registry/templates/startCompose.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 echo Bring compose up
-docker-compose   -f {{nexus_compose_dir}}/docker-compose.yml up -d  --build --force-recreate --remove-orphans
+docker compose   -f {{nexus_compose_dir}}/docker-compose.yml up -d  --build --force-recreate --remove-orphans
