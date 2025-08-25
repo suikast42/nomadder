59738b7f90142030aea33975ba9fd4fc3d9b021e
VURU <sueleyman.vurucu@amova.eu>
2025-08-25 11:37:54 +0200

    Version updates: nexus 3.76.1 -> 3.83.1 vector 0.48.0 -> 0.49.0 consul 1.21.3 -> 1.21.4 nomad 1.10.3 -> 1.10.4 alloy 1.10.1 -> 1.10.2 consul_cni 1.8.0 -> 1.8.1 grafana 12.1.0 -> 12.1.1 nats_server 2.11.7 -> 2.11.8 grafana_mimir 2.16.1 -> 2.17.0 envoy 1.35.0 -> 1.35.1 keycloak 26.3.2 -> 26.3.3 nats cli nsc 2.11.0 -> 2.11.1

--- ansible/setup/stack_observability/templates/grafana/installPlugins.sh
+++ ansible/setup/stack_observability/templates/grafana/installPlugins.sh
@@ -42,7 +42,10 @@ grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install gapit-htmlgraphics-p
 grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install mckn-funnel-panel &&
 
 grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install  grafana-pyroscope-app &&
-grafana cli --pluginsDir=$GF_PATHS_PLUGINS  --pluginUrl=https://storage.googleapis.com/integration-artifacts/grafana-lokiexplore-app/grafana-lokiexplore-app-latest.zip plugins install grafana-lokiexplore-app &&
-grafana cli --pluginsDir=$GF_PATHS_PLUGINS  --pluginUrl=https://storage.googleapis.com/integration-artifacts/grafana-exploretraces-app/grafana-exploretraces-app-latest.zip plugins install grafana-traces-app &&
+grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install  grafana-lokiexplore-app  &&
+grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install  grafana-metricsdrilldown-app  &&
+grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install  grafana-exploretraces-app  &&
+grafana cli --pluginsDir=$GF_PATHS_PLUGINS  plugins install  grafana-investigations-app  &&
+
 
 echo All plugins are installed
