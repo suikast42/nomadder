d83f26f76bd7812f513e6c1d8f89e364618270b7
vuru <sueleyman.vurucu@sms-group.com>
2026-09-11 09:41:49 +0200

    Dev cluster configs

--- ansible/environment/local_devops/setenv.sh
+++ ansible/environment/local_devops/setenv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export DOCKER_HOST=192.168.30.121
+#export DOCKER_HOST=192.168.30.121
 export TLS_SAN=tdp.private
-export ENVIRONMENT="/mnt/c/IDE/Projects_Git/playground/nomadder/ansible/environment/local_devops"
+export ENVIRONMENT="~/projects/bi/nomadder/ansible/environment/local_devops"
 export ANSIBLE_CONFIG="$ENVIRONMENT/../ansible.cfg"
 export ANSIBLE_INVENTORY="$ENVIRONMENT/inventory/hosts.ini"
 export ANSIBLE_DEBUG=False
-export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
-export PULL_REGISTRY="registry.$TLS_SAN"
-export PUSH_REGISTRY="$DOCKER_HOST:5001"
+#export DOCKER_CERT_PATH="$ENVIRONMENT/docker_client"
+#export PULL_REGISTRY="registry.$TLS_SAN"
+#export PUSH_REGISTRY="$DOCKER_HOST:5001"
